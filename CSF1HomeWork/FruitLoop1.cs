e1e3be2a6049d5e1b849d3c99659869453bf8f31
William McCarty <kcchief@yahoo.com>
2021-07-25 00:09:01 -0500

    Added the FruitLoop1 Homework

---
+++ CSF1HomeWork/FruitLoop1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSF1HomeWork
+{
+    class FruitLoop1
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "-==== FRUITLOOP1 ====-";
+
+            string[] fruits = { "Grapefruit", "Grapes", "Apple", "Mango", "Blueberries", "Apricot" };
+
+            for (int i = 0; i < fruits.Length; i++)
+            {
+                Console.WriteLine(fruits[i]);
+            }
+        }//END MAIN
+    }//END CLASS
+}//END NAMESPACE
